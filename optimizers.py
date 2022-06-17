22bed3bcdc45a08fda24367850a609264fb1d4ca
mnabegh <mahmoudnabegh@gmail.com>
2022-06-17 10:16:07 +0200

    add json dumps and edit the scripts

--- optimizers.py
+++ optimizers.py
@@ -1,25 +1,101 @@
 import numpy as np
-import pickle
-import os
 import matplotlib.pyplot as plt
-import matplotlib
-import networkx
-from utils import consensus_distance
+from utils import consensus_distance, return_step_size, sampling, sampling_s, sampling_grid, shuffling, shuffling_back
+from topology import FixedMixingMatrix
 
-def optimize_decentralized(X, topology, A, B, gamma, sigma, num_iter=100):
-    # X.shape = (num_dim, num_nodes)
-    # A.shape = (num_nodes, num_dim, num_dim)
-    # B.shape = (num_nodes, num_dim)
-    
+# probability of node failing 
+def optimize_decentralized(X, A, B, zeta, sigma, sampling_method, sampling_param, shuffle, num_iter): 
+    # getting dim & number of nodes 
     num_dim, num_nodes = X.shape
-    X_iter = np.copy(X)
-    errors = [consensus_distance(X_iter, A, B)]
-    for i in range(0, num_iter):
-        W = topology(i)
-        AXmB = (np.einsum("ijk,ik->ij", A, X_iter.T) - B) # shape (num_nodes, num_dim)
-        grad = np.einsum("ijk,ij->ik", A, AXmB) # shape (num_nodes, num_dim)
-        noise = np.random.normal(0, np.sqrt(sigma / num_dim), size=X.shape)
-        X_iter = X_iter - gamma * (grad.T + noise)
-        X_iter = X_iter.dot(W)
-        errors += [consensus_distance(X_iter, A, B)]
+    topology_str = ["ring", "centralized", "grid"]
+    errors = {}
+    for curr_topology in topology_str:
+      X_iter = np.copy(X)
+      errors[curr_topology] = [consensus_distance(X_iter, A, B)]
+      
+      gamma = return_step_size((curr_topology, zeta, sigma))
+      for i in range(0, num_iter):
+          AXmB = (np.einsum("ijk,ik->ij", A, X_iter.T) - B) # shape (num_nodes, num_dim)
+          grad = np.einsum("ijk,ij->ik", A, AXmB) # shape (num_nodes, num_dim)
+
+          if sampling_method == "failure_prob":
+            thr = sampling_param
+            # sampled index, sub X, and grad
+            if curr_topology is not "grid":        
+              sampledIndex, X_curr, grad_curr = sampling(thr, num_nodes, X_iter, grad)
+            else:
+              sampledIndex, X_curr, grad_curr = sampling_grid(thr, num_nodes, X_iter, grad)
+
+          elif sampling_method == "fixed_s":
+            s = sampling_param
+            assert int(np.sqrt(s)) ** 2 == s, "number of workers should be square of an int"
+            sampledIndex, X_curr, grad_curr = sampling_s(s, num_nodes, X_iter, grad)
+          numberOfSampled = np.sum(sampledIndex)
+          # create W 
+          if numberOfSampled != 0:
+            topology = FixedMixingMatrix(curr_topology, numberOfSampled)
+            W_curr = topology(i)
+            noise = np.random.normal(0, np.sqrt(sigma / num_dim), size=X_curr.shape)
+            if shuffle:
+              # shuffling 
+              index, X_curr_sh, grad_curr_sh = shuffling(X_curr, grad_curr)
+              # update
+              X_temp_sh = X_curr_sh - gamma * (grad_curr_sh.T + noise)
+              X_next_sh = X_temp_sh.dot(W_curr)
+              # shuffling_back 
+              X_next = shuffling_back(index, X_next_sh)
+            else:
+              X_temp = X_curr - gamma * (grad_curr.T + noise)
+              X_next = X_temp.dot(W_curr)
+            X_iter[:, sampledIndex] = X_next
+          errors[curr_topology] += [consensus_distance(X_iter, A, B)]
+            # print('X_next:', X_next)
+    return errors, X_iter
+
+# probability of node failing 
+def optimize_decentralized_one_topology(X, A, B, sigma, sampling_method, sampling_param, shuffle, num_iter, topology_str, gamma): 
+    # getting dim & number of nodes 
+    num_dim, num_nodes = X.shape
+    errors = {}
+    for curr_topology in topology_str:
+      X_iter = np.copy(X)
+      errors[curr_topology] = [consensus_distance(X_iter, A, B)]
+      
+      # gamma = return_step_size((curr_topology, zeta, sigma))
+      for i in range(0, num_iter):
+          AXmB = (np.einsum("ijk,ik->ij", A, X_iter.T) - B) # shape (num_nodes, num_dim)
+          grad = np.einsum("ijk,ij->ik", A, AXmB) # shape (num_nodes, num_dim)
+
+          if sampling_method == "failure_prob":
+            thr = sampling_param
+            # sampled index, sub X, and grad
+            if curr_topology is not "grid":        
+              sampledIndex, X_curr, grad_curr = sampling(thr, num_nodes, X_iter, grad)
+            else:
+              sampledIndex, X_curr, grad_curr = sampling_grid(thr, num_nodes, X_iter, grad)
+
+          elif sampling_method == "fixed_s":
+            s = sampling_param
+            assert int(np.sqrt(s)) ** 2 == s, "number of workers should be square of an int"
+            sampledIndex, X_curr, grad_curr = sampling_s(s, num_nodes, X_iter, grad)
+          numberOfSampled = np.sum(sampledIndex)
+          # create W 
+          if numberOfSampled != 0:
+            topology = FixedMixingMatrix(curr_topology, numberOfSampled)
+            W_curr = topology(i)
+            noise = np.random.normal(0, np.sqrt(sigma / num_dim), size=X_curr.shape)
+            if shuffle:
+              # shuffling 
+              index, X_curr_sh, grad_curr_sh = shuffling(X_curr, grad_curr)
+              # update
+              X_temp_sh = X_curr_sh - gamma * (grad_curr_sh.T + noise)
+              X_next_sh = X_temp_sh.dot(W_curr)
+              # shuffling_back 
+              X_next = shuffling_back(index, X_next_sh)
+            else:
+              X_temp = X_curr - gamma * (grad_curr.T + noise)
+              X_next = X_temp.dot(W_curr)
+            X_iter[:, sampledIndex] = X_next
+          errors[curr_topology] += [consensus_distance(X_iter, A, B)]
+            # print('X_next:', X_next)
     return errors, X_iter
--- utils.py
+++ utils.py
@@ -1,9 +1,4 @@
 import numpy as np
-import pickle
-import os
-import matplotlib.pyplot as plt
-import matplotlib
-import networkx
 
 
 def generate_functions(num_nodes, num_dim, zeta):
@@ -33,3 +28,93 @@ def consensus_distance(X, A, B): # ||x-x*||^2
     dist = [np.linalg.norm(X[:,i] - x_star) ** 2 for i in range(0, num_nodes)]
     return np.mean(dist)
 
+def return_step_size(parameters_tuple):
+  """
+  This function returns the step size suitable for each combination of parameters to produce the best results for each experiment
+  These step sizes returned were tuned manually
+  """
+
+  # lists of value parameters we support
+  list_of_topologies = ["ring", "centralized", "grid"]
+  list_of_zetas  = [0, 1, 10]
+  list_of_sigmas = [0, 1, 100]
+
+  assert parameters_tuple[0] in list_of_topologies, f"toplogy should be inside {list_of_topologies}"
+  assert parameters_tuple[1] in list_of_zetas, f"zeta should be inside {list_of_zetas}"
+  assert parameters_tuple[2] in list_of_sigmas, f"sigma should be inside {list_of_sigmas}"
+
+  step_size_mapper = {(list_of_topologies[0], list_of_zetas[0], list_of_sigmas[0]): 0.05,
+                      (list_of_topologies[0], list_of_zetas[0], list_of_sigmas[1]): 0.003,
+                      (list_of_topologies[0], list_of_zetas[0], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[0], list_of_zetas[1], list_of_sigmas[0]): 0.00012,
+                      (list_of_topologies[0], list_of_zetas[1], list_of_sigmas[1]): 0.0001,
+                      (list_of_topologies[0], list_of_zetas[1], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[0], list_of_zetas[2], list_of_sigmas[0]): 0.000065,
+                      (list_of_topologies[0], list_of_zetas[2], list_of_sigmas[1]): 0.00005,
+                      (list_of_topologies[0], list_of_zetas[2], list_of_sigmas[2]): 0.0001,
+                      
+                      (list_of_topologies[1], list_of_zetas[0], list_of_sigmas[0]): 0.05, 
+                      (list_of_topologies[1], list_of_zetas[0], list_of_sigmas[1]): 0.007,
+                      (list_of_topologies[1], list_of_zetas[0], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[1], list_of_zetas[1], list_of_sigmas[0]): 0.01,
+                      (list_of_topologies[1], list_of_zetas[1], list_of_sigmas[1]): 0.005,
+                      (list_of_topologies[1], list_of_zetas[1], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[1], list_of_zetas[2], list_of_sigmas[0]): 0.01,
+                      (list_of_topologies[1], list_of_zetas[2], list_of_sigmas[1]): 0.003,
+                      (list_of_topologies[1], list_of_zetas[2], list_of_sigmas[2]): 0.0001,
+
+                      (list_of_topologies[2], list_of_zetas[0], list_of_sigmas[0]): 0.05, 
+                      (list_of_topologies[2], list_of_zetas[0], list_of_sigmas[1]): 0.005,
+                      (list_of_topologies[2], list_of_zetas[0], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[2], list_of_zetas[1], list_of_sigmas[0]): 0.001,
+                      (list_of_topologies[2], list_of_zetas[1], list_of_sigmas[1]): 0.0015,
+                      (list_of_topologies[2], list_of_zetas[1], list_of_sigmas[2]): 0.0001,
+                      (list_of_topologies[2], list_of_zetas[2], list_of_sigmas[0]): 0.0005,
+                      (list_of_topologies[2], list_of_zetas[2], list_of_sigmas[1]): 0.0005,
+                      (list_of_topologies[2], list_of_zetas[2], list_of_sigmas[2]): 0.0001,
+                      }
+
+  return step_size_mapper[parameters_tuple]
+
+  def sampling(thr, num_nodes, X, grad): #sampledIndex, X_curr
+    # vector consisting of samples from uniform distribution
+    sample = np.random.uniform(low = 0.0, high = 1.0, size = (num_nodes))
+    sampledIndex = sample >= thr
+    X_curr = X[:, sampledIndex]
+    grad_curr = grad[sampledIndex, :]
+    return sampledIndex, X_curr, grad_curr
+
+def sampling_grid(thr, num_nodes, X, grad):
+  # vector consisting of samples from uniform distribution
+  sample = np.random.uniform(low = 0.0, high = 1.0, size = (num_nodes))
+  sampledIndex = sample >= thr
+  while (int(np.sqrt(sampledIndex.sum())) ** 2) != (sampledIndex.sum()):
+    sample = np.random.uniform(low = 0.0, high = 1.0, size = (num_nodes))
+    sampledIndex = sample >= thr
+  X_curr = X[:, sampledIndex]
+  grad_curr = grad[sampledIndex, :]
+  return sampledIndex, X_curr, grad_curr
+
+def sampling_s(n_sampled_nodes, n_total_nodes, X, grad):
+  n_total_nodes_line = np.arange(n_total_nodes)
+  choices = np.random.choice(n_total_nodes_line, n_sampled_nodes, replace=False)
+  sampledIndex = np.array([False] * n_total_nodes)
+  sampledIndex[choices] = True
+  X_curr = X[:, sampledIndex]
+  grad_curr = grad[sampledIndex, :]
+  return sampledIndex, X_curr, grad_curr
+
+
+def shuffling(X_curr, grad_curr):
+  num_dim, num_sampled_nodes = X_curr.shape
+  index = np.arange(num_sampled_nodes)
+  np.random.shuffle(index)
+  X_curr_sh = X_curr.T[index].T
+  grad_curr_sh = grad_curr[index]
+  return index, X_curr_sh, grad_curr_sh
+
+def shuffling_back(index, X_curr_sh):
+  X_curr_indexed = np.concatenate((index.reshape(1, len(index)), X_curr_sh), axis = 0)
+  X_curr_indexed = X_curr_indexed[:, X_curr_indexed[0, :].argsort()]
+  X_curr_indexed = X_curr_indexed[1:, :]
+  return X_curr_indexed 
